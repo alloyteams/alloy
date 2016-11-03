f483897936189f50a93f5db57d754c45223f7a88
reedv <villanuevareed@gmail.com>
2016-11-02 22:20:05 -1000

    fixed 'undefined username' error on login (reed's machine)
    Can now also access User and Projects collection data. For some reason,
    when calling onUserCreate(), user.username was undefined. Changed to
    user.services.cas.id to fix.

--- app/imports/startup/server/accounts.js
+++ app/imports/startup/server/accounts.js
@@ -1,15 +1,19 @@
-import { Meteor } from 'meteor/meteor';
-import { Accounts } from 'meteor/accounts-base';
+import {Meteor} from 'meteor/meteor';
+import {Accounts} from 'meteor/accounts-base';
 import {Projects, ProjectsSchema} from '../../api/projects/projects.js';
 import {Users, UsersSchema} from '../../api/users/users.js';
-import { _ } from 'meteor/underscore';
+import {_} from 'meteor/underscore';
 
 /* eslint-disable no-console */
 
 
-/* Validate username, sending a specific error message on failure. */
+/* Validate username against UH cas, sending a specific error message on failure. */
 Accounts.validateNewUser(function (user) {
   if (user) {
+    // get username from uh-cas login service
+    console.log(user.services)
+    console.log(user.services.cas)
+    console.log(user.services.id)
     const username = user.services.cas.id;
     if (username && _.contains(Meteor.settings.allowed_users, username)) {
       return true;
@@ -18,10 +22,10 @@ Accounts.validateNewUser(function (user) {
   throw new Meteor.Error(403, 'User not in the allowed list');
 });
 
-
 if (!Meteor.settings.cas) {
   console.log('CAS settings not found! Hint: "meteor --settings ../config/settings.development.json"');
 }
+
 
 Accounts.onCreateUser(function (options, user) {
   /* From http://docs.meteor.com/api/accounts.html
@@ -30,15 +34,15 @@ Accounts.onCreateUser(function (options, user) {
 
    1. username: a unique String identifying the user.
    2. emails: an Array of Objects with keys address and verified; an email address
-      may belong to at most one user. verified is a Boolean which is true if the user has
-      verified the address with a token sent over email.
+   may belong to at most one user. verified is a Boolean which is true if the user has
+   verified the address with a token sent over email.
    3. createdAt: the Date at which the user document was created.
    4. profile: an Object which the user can create and update with any data. Do not store
-      anything on profile that you wouldn’t want the user to edit unless you have a deny
-      rule on the Meteor.users collection.
+   anything on profile that you wouldn’t want the user to edit unless you have a deny
+   rule on the Meteor.users collection.
    5. services: an Object containing data used by particular login services. For example,
-      its reset field contains tokens used by forgot password links, and its resume field
-      contains tokens used to keep you logged in between sessions.
+   its reset field contains tokens used by forgot password links, and its resume field
+   contains tokens used to keep you logged in between sessions.
    * */
 
   /* initialize a new user */
@@ -57,19 +61,20 @@ Accounts.onCreateUser(function (options, user) {
   };
   // if the default project has not yet been added to Projects collection, do so.
   // returns 'undefined' if none found (falsey), else first matched obj. (truthy?)
-  let defaultExists = Projects.findOne({projectName: 'The Null Project'});
-  if(!(defaultExists)){
+  let defaultExists = Projects.findOne({ projectName: 'The Null Project' });
+  if (!(defaultExists)) {
     Projects.insert(defaultProject);
   }
 
   // initialize newUser account info ad add to Users collection
   const newUser = {
-    username: user.username,
+    username: user.services.cas.id,  // if not using UH cas, use: user.username
     projects: [defaultProject.projectName],
     events: ['The Null Event-1', 'The Null Event-2'],
     adminProjects: [defaultProject.projectName],
     isSiteAdmin: false
   };
+  console.log(newUser)
   Users.insert(newUser);  // this means User documents will have different _id than the Meteor.user._id
                           // unless we create our own _id or Meteor.users.
                           // see https://guide.meteor.com/accounts.html#adding-fields-on-registration
@@ -78,8 +83,8 @@ Accounts.onCreateUser(function (options, user) {
   // add this user as a member and admin of the default project
   // see https://docs.mongodb.com/manual/reference/operator/update/
   // TODO: add function to Projects collection api that allows user.projects and project.members to be set simultaneously
-  Projects.update({projectName: 'The Null Project'}, { $addToSet: {members: newUser.username} });
-  Projects.update({projectName: 'The Null Project'}, { $addToSet: {admins: newUser.username} });
+  Projects.update({ projectName: 'The Null Project' }, { $addToSet: { members: newUser.username } });
+  Projects.update({ projectName: 'The Null Project' }, { $addToSet: { admins: newUser.username } });
 
 
   // We still want the default hook's 'profile' behavior.
@@ -89,16 +94,15 @@ Accounts.onCreateUser(function (options, user) {
   return user;
 });
 
-
-
-/* When running app for first time, pass a settings file to set up a default user account. */
-if (Meteor.users.find().count() === 0) {
-  if (!!Meteor.settings.defaultAccount) {
-    Accounts.createUser({
-      username: Meteor.settings.defaultAccount.username,
-      password: Meteor.settings.defaultAccount.password,
-    });
-  } else {
-    console.log('No default user!  Please invoke meteor with a settings file.');
-  }
-}
+// FIXME: causes 'id required' error when using UH accounts-cas. Should just delete?
+/* When running app for first time, pass a settings file to set up a default user account if no other users. */
+// if (Meteor.users.find().count() === 0) {
+//   if (!!Meteor.settings.defaultAccount) {
+//     Accounts.createUser({
+//       username: Meteor.settings.defaultAccount.username,
+//       password: Meteor.settings.defaultAccount.password,
+//     });
+//   } else {
+//     console.log('No default user!  Please invoke meteor with a settings file.');
+//   }
+// }
