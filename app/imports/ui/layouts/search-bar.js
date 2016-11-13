e93df87a4dd7112af6993a15ed5f76fdc150327d
Neil Teves <tevesnc@gmail.com>
2016-11-13 11:00:12 -1000

    Added the search functionality to search page

---
+++ app/imports/ui/layouts/search-bar.js
@@ -0,0 +1,32 @@
+/**
+ * Created by neil on 11/10/2016.
+ */
+
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { Projects } from '../../api/projects/projects.js';
+
+
+Template.search.onCreated(function appBodyOnCreated() {
+  // placeholder: typically you will put global subscriptions here if you remove the autopublish package.
+});
+
+Template.search.helpers({
+  // placeholder: if you display dynamic data in your layout, you will put your template helpers here.
+})
+
+Template.search.events({
+  'submit .form-register': function (event, template) {
+    event.preventDefault();
+    let getInput = event.target.searchInput.value;
+
+    console.log('search input: ' + getInput);
+
+    let myCursor = Projects.find({skills: getInput});
+
+    console.log(Projects.find().count());
+    console.log(Meteor.users.find().count());
+
+    console.log(myCursor.fetch());
+  },
+});
