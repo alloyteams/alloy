e0f06990776e1481e8c5cae9481fe0f15395c022
alliekim13 <alliekim@hawaii.edu>
2016-11-29 00:09:09 -1000

    Added link to user's profile page to menu bar

--- app/imports/ui/layouts/header.js
+++ app/imports/ui/layouts/header.js
@@ -1,4 +1,5 @@
 import {Template} from 'meteor/templating';
+import {Users, UsersSchema} from '../../api/users/users.js';
 
 // The Header menu does not use dropdown menus, but most menus do.
 // Here's how to do the required initialization for Semantic UI dropdown menus.
@@ -6,9 +7,23 @@ Template.Header.onRendered(function enableDropDown() {
   this.$('.dropdown').dropdown();
 });
 
-// Here's how to do the required initialization for Semantic UI dropdown menus.
-Template.Header.onRendered(function enableDropDown() {
-  this.$('.dropdown').dropdown({
-    action: 'select',
+Template.Header.onCreated(function onCreated() {
+  this.autorun(() => {
+    this.subscribe('Users');
   });
-});
+  });
+
+// Here's how to do the required initialization for Semantic UI dropdown menus.
+  Template.Header.onRendered(function enableDropDown() {
+    this.$('.dropdown').dropdown({
+      action: 'select',
+    });
+  });
+
+  Template.Header.helpers({
+    getMyId: function() {
+      const userName = Meteor.user().profile.name;
+      const userNameId = Users.find({ 'username': userName }).fetch()[0]['_id'];
+      return userNameId;
+    },
+  });
