2914e60b8fa735d250a5b4616a490c39d82fd9b3
Ed White <ew@spyhi.com>
2017-04-11 19:27:37 -1000

    Fixed merge conflicts with mail
    Fixed merge conflicts with mail branch

--- app/imports/startup/server/index.js
+++ app/imports/startup/server/index.js
@@ -1,8 +1,5 @@
 import './accounts.js';
 import './publications.js';
 import './categories-dict.js';
-<<<<<<< HEAD
 import './slingshot.js'; // AWS Uploader service definitions
-=======
 import './mail-url.js';
->>>>>>> refs/remotes/origin/master
